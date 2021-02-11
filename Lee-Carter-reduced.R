9fae385b3644acd42a11cecb935921cb2d3526cf
Helene Behrens <helen@macbook-air.home>
2021-02-11 12:57:15 +0100

    Add file for reduced Lee-Carter model. Get better results for epsilon and precisions (hyperparameters), but very bad result for kappa.

--- Lee-Carter-reduced.R
+++ Lee-Carter-reduced.R
@@ -1,3 +1,5 @@
+# reduced version of Lee-Carter model
+
 #   ----  first attempt at inference of Lee-Carter models for mortality 
 #  predictions with inlabru  ----   
 
@@ -6,10 +8,15 @@ library(inlabru)
 library(ggplot2)
 library(patchwork)
 
-N = 10000
+N = 1000
 
-x = sample(1:100, N, replace = TRUE)   # 1000 samples of ages 1-100
-t = sample(1:100, N, replace = TRUE)   # 1000 samples of years 1901-2000: represented as 1-100
+nx = 10
+nt = 10
+
+at.risk = 1000
+
+x = sample(1:nx, N, replace = TRUE)   # 1000 samples of ages 1-100
+t = sample(1:nt, N, replace = TRUE)   # 1000 samples of years 1901-2000: represented as 1-100
 
 obs = data.frame(x,t)
 
@@ -17,9 +24,9 @@ obs = data.frame(x,t)
 
 tau.iid = 1/0.1**2   #  Standard deviation of 0.1 for the iid effects (beta)
 tau.rw = 1/0.2**2    #  Standard deviation of 0.2 for the random walk
-tau.epsilon = 1/0.05**2   #  Standard deviation of 0.05 for the noise 
+tau.epsilon = 1/0.1**2   #  Standard deviation of 0.05 for the noise 
 
-kappa = -cos(((1:100 - 20)* pi)/80)
+kappa = -cos(((1:nt - 3)* pi)/6)
 kappa = kappa - mean(kappa)
 
 alpha = -2.0   #  Intercept - perhaps make this iid in the future? 
@@ -27,19 +34,19 @@ alpha = -2.0   #  Intercept - perhaps make this iid in the future?
 phi = 0.025  #  Drift of random walk
 
 #  sample synthetic data:
-beta = rnorm(100, 0, sqrt(1/tau.iid))  # should it not depend on t??
-beta = 1/100 + beta - mean(beta)   # sum to 1
+beta = rnorm(nx, 0, sqrt(1/tau.iid))  # should it not depend on t??
+beta = 1/nx + beta - mean(beta)   # sum to 1
 
-beta.x = beta[x]
+#beta.x = beta[x]
 
-phi.t = phi*t
+#phi.t = phi*t
 
-kappa.t = kappa[t]
+#kappa.t = kappa[t]
 
-m.epsilon = matrix(rnorm(100*100, 0, sqrt(1/tau.epsilon)), 100, 100)
+m.epsilon = matrix(rnorm(nx*nt, 0, sqrt(1/tau.epsilon)), nx, nt)
 # how to structure samples from these?  
 
-eta = alpha + beta.x*phi.t + beta.x*kappa.t + m.epsilon[x,t]  # is this the correct representation of eta?
+#eta = alpha + beta.x*phi.t + beta.x*kappa.t + m.epsilon[x,t]  # is this the correct representation of eta?
 # no, you now have all combinations of the 1000 samples of x and t each. You want the grid representation. 
 
 # add everything to the dataframe instead of specifying beta.x and kappa.t etc separately. 
@@ -57,12 +64,15 @@ epsilon.map <- function(x,t){
 
 obs = cbind(obs, epsilon = apply(obs, 1, function(row) epsilon.map(row['x'], row['t'])))
 
-eta.func <- function(alpha, beta, phit, kappa){
-  return(alpha + beta*phit + beta*kappa)
+eta.func <- function(alpha, beta, phit, kappa, epsilon){
+  return(alpha + beta*phit + beta*kappa + epsilon)
 }
 
-obs = cbind(obs, eta = apply(obs, 1, function(row) eta.func(row['alpha'], row['beta'], row['phi.t'], row['kappa'])))
-obs = cbind(obs, at.risk = rep(1000, N))  # add constant at risk:
+obs = cbind(obs, eta = apply(obs, 1,
+                             function(row) eta.func(row['alpha'], row['beta'],
+                                                    row['phi.t'], row['kappa'],
+                                                    row['epsilon'])))
+obs = cbind(obs, at.risk = rep(at.risk, N))  # add constant at risk
 
 # sample actual observations:
 y = rpois(N, obs$at.risk*exp(obs$eta))
@@ -78,19 +88,19 @@ xt.func <- function(x,t){
 obs = cbind(obs, xt = apply(obs, 1, function(row) xt.func(row['x'], row['t'])))
 
 # plot observations:
-ggplot(data = obs, aes(x=x, y=t, fill = y)) + geom_tile()
+ggplot(data = obs, aes(x=t, y=x, fill = y)) + geom_tile()
 
 
 #   ----  Start defining the inlabru model components  ----   
 
 #  helper values for constraining of beta:
-A.mat = matrix(1, nrow = 1, ncol = 100)  #  not sure if you did this correctly
+A.mat = matrix(1, nrow = 1, ncol = nx)  #  not sure if you did this correctly
 e.vec = 1
 
 comp = ~ Intercept + 
   phi(t, model = "linear") + 
   beta(x, model = "iid", extraconstr = list(A = A.mat, e = e.vec)) + 
-  kappa(t1, model = "rw1", values = 1:100, constr = TRUE) + 
+  kappa(t1, model = "rw1", values = 1:nt, constr = TRUE) + 
   epsilon(xt, model = "iid")  # change from iid to rw1
 
 form.1 = y ~ Intercept + beta*phi + beta*kappa + epsilon
@@ -129,7 +139,7 @@ gg.epsilon.true
 data.epsilon = res$summary.random$epsilon
 data.epsilon = cbind(data.epsilon, x = apply(data.epsilon,1, function(row) row['ID']%/%100 ))
 data.epsilon = cbind(data.epsilon, t = apply(data.epsilon, 1, function(row) row['ID']%%100))
-gg.epsilon = ggplot(data = data.epsilon, aes(x = x, y = t, fill = mean)) + geom_tile()
+gg.epsilon = ggplot(data = data.epsilon, aes(x = x, y = t, fill = mean)) + geom_tile() + ggtitle("Simulated epsilon")
 gg.epsilon
 
 # density plots of true and simulated epsilon:
