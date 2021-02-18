1dca47a8bcc40c01be5d125f1f94ed39cf5973e9
Helene Behrens <helen@dhcp-10-22-46-214.wlan.ntnu.no>
2021-02-18 13:02:35 +0100

    Initialize folder and first R script for inference of Lee-Carter models extended with a cohort effect.

---
+++ Lee-Carter-cohort-effect/L-C-cohort-first-attempt.R
@@ -0,0 +1,4 @@
+# R-script for first attempt at inference with inlabru of 
+# Lee-Carter models with a cohort effect. 
+# The model is inspired by Wiśniowski, A., Smith, P.W.F., Bijak, J. et al., in 
+# Bayesian Population Forecasting: Extending the Lee-Carter Method.
